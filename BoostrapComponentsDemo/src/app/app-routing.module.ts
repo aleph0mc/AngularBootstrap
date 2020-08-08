241871adb9cb410922a1974fb9a762c3f3ed2108
aleph0mc <aleph0mc@gmail.com>
2020-08-08 15:12:42 +0200

    first commit

---
+++ BoostrapComponentsDemo/src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { FormComponent } from './form/form.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      {
+        path: 'form',
+        component: FormComponent,
+      },
+    ],
+  },
+  { path: '**', redirectTo: '', pathMatch: 'full' },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
