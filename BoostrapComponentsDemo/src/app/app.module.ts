6324055648afe1ad9360a7de9df51c2c38620435
Milko Lupinacci <mlupinacci@Citadel.local>
2020-10-07 08:49:25 +0200

    added components

--- BoostrapComponentsDemo/src/app/app.module.ts
+++ BoostrapComponentsDemo/src/app/app.module.ts
@@ -2,9 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ModalModule } from 'ngx-bootstrap/modal';
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+// required for ngx-bootstrap
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// ngx-bootstrap component
 import { TabsModule } from 'ngx-bootstrap/tabs';
 import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
 
 // services
 import { AlertifyService } from './_services/alertify.service';
@@ -43,9 +50,13 @@ import { TabsetComponent } from './tabset/tabset.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
     ModalModule.forRoot(),
     TabsModule.forRoot(),
     TooltipModule.forRoot(),
+    BsDropdownModule.forRoot(),
+    BsDatepickerModule.forRoot(),
   ],
   providers: [AlertifyService],
   bootstrap: [AppComponent],
--- BoostrapComponentsDemo/src/app/form/form.component.ts
+++ BoostrapComponentsDemo/src/app/form/form.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
-  styleUrls: ['./form.component.css']
+  styleUrls: ['./form.component.css'],
 })
 export class FormComponent implements OnInit {
+  reactiveForm: FormGroup;
+  ddlItems: any = [
+    { name: 'Rome', value: 1 },
+    { name: 'Milan', value: 2 },
+    { name: 'Valletta', value: 3 },
+    { name: 'Turin', value: 4 },
+  ];
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
+    this.initializeReactiveForm();
   }
 
+  initializeReactiveForm(): void {
+    this.reactiveForm = this.fb.group({
+      ddlSelected: [''],
+      dateDMY: [new Date()],
+    });
+  }
+
+  submitForm(): void {
+    console.log('ddlValue' + this.reactiveForm.get('ddlSelected').value);
+    console.log(this.reactiveForm.get('dateDMY').value);
+  }
 }
